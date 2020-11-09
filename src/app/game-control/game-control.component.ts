b8ace86aef64e40064e04883862505fd757aa740
Bolivar Oscar Berrio Mayers <berriobolivar@gmail.com>
2020-11-09 15:15:32 -0500

    Displaying even and odd word

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent implements OnInit {
+  @Output() oddNumber = new EventEmitter<{ number: number }>();
+  @Output() evenNumber = new EventEmitter<{ number: number }>();
+  interval: number;
+  count = 0;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  onGameStart() {
+    this.interval = setInterval(() => {
+      this.count++;
+      if (this.count % 2 === 0) {
+        this.oddNumber.emit(this.count);
+      } else {
+        this.evenNumber.emit(this.count);
+      }
+    }, 1000);
+  }
+
+  onGameStop() {
+    clearInterval(this.interval);
   }
 
 }
